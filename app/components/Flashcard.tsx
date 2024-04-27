0430e327827046d4873e72ae0428a7575fe6861e
Joe Gallegos <j.gallegos25u@gmail.com>
2024-04-26 19:19:43 -0600

    initial question logic

--- app/components/Flashcard.tsx
+++ app/components/Flashcard.tsx
@@ -1,41 +1,21 @@
 'use client';
 import React, { useState } from 'react';
-import { questionObject } from '../questions';
 
-const Flashcard = () => {
-  const [showAnswer, setShowAnswer] = useState(false);
-
+const Flashcard = ({ title, question, id, options, answer }: any) => {
   return (
-    <div className="max-w-lg rounded overflow-hidden shadow-lg">
-      {questionObject.map(question => (
-        <div key={question.id}>
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{question.title}</div>
-            <p className=" text-base">{question.question}</p>
-            <h2 className="underline text-center uppercase mt-3">Options</h2>
-            {question.options.map(q => (
-              <>
-                <li>{q}</li>
-              </>
-            ))}
-            <button
-              className="outline-white"
-              onClick={() => setShowAnswer(!showAnswer)}
-            >
-              Reveal Answer
-            </button>
-            <div>
-              {showAnswer ? (
-                <span className="text-gray-700 text-base">
-                  {question.answer}
-                </span>
-              ) : (
-                ''
-              )}
-            </div>
-          </div>
+    <div className="max-w-lg rounded overflow-hidden shadow-lg bg-slate-500">
+      <div key={id}>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{title}</div>
+          <p className=" text-base">{question}</p>
+          <h2 className="underline text-center uppercase mt-3">Options</h2>
+          {options.map((o: string) => (
+            <>
+              <li>{o}</li>
+            </>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,60 @@
+'use client';
 import Flashcard from './components/Flashcard';
+import { questionObject } from './questions';
+import { useState } from 'react';
+
+const getRandomObject = (array: string | any[]) => {
+  const randomObject = array[Math.floor(Math.random() * array.length)];
+  return Array(randomObject);
+};
 
 export default function Home() {
+  const [randomData, setRandomData] = useState(() =>
+    getRandomObject(questionObject),
+  );
+  const [showAnswer, setShowAnswer] = useState(false);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>This is the start of my mechanics testing helper page thingy</h1>
-        <Flashcard />
+        <h1 className="text-center">Brunswick A2 C-Mechanic Helper</h1>
+        {randomData.map(
+          (question: {
+            id: any;
+            question: string;
+            title: any;
+            answer: string;
+            options: any;
+          }) => (
+            <>
+              <Flashcard
+                key={question.id}
+                title={question.title}
+                question={question.question}
+                answer={question.answer}
+                options={question.options}
+              />
+              <button onClick={() => setShowAnswer(!showAnswer)}>
+                Show Answer
+              </button>
+              <div>
+                {showAnswer ? (
+                  <span className="text-base">{question.answer}</span>
+                ) : (
+                  ''
+                )}
+              </div>
+            </>
+          ),
+        )}
+        <button
+          onClick={() => {
+            setRandomData(getRandomObject(questionObject));
+            setShowAnswer(false);
+          }}
+        >
+          Next Question
+        </button>
       </div>
     </main>
   );
