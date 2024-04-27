16144143e21f2e0202d716f4bedd8956e7607a3a
Joe Gallegos <j.gallegos25u@gmail.com>
2024-04-26 21:53:32 -0600

    finish questions

--- app/questions.ts
+++ app/questions.ts
@@ -162,4 +162,239 @@ export const questionObject = [
     ],
     answer: 'Turn pan, Cross Conveyor',
   },
+  {
+    id: 15,
+    title: 'Blackout',
+    question:
+      'What is the first thing you should do when a pinsetter shuts itself off electronically by a deck or turret jam?',
+    options: [
+      "Clear the call quickly so the bowler doesn't have to wait.",
+      'Shut the machine off and clear call.',
+      'Lockout/Tagout the machine.',
+      'Pull the pin out of the deck chutes',
+    ],
+    answer: 'Lockout / Tagout the machine',
+  },
+  {
+    id: 16,
+    title: 'Blackout',
+    question:
+      'What two safety switches will automatically shut off the pinsetter in the event of a pin jam?',
+    options: [
+      'Deck jam switch, turret jam switch',
+      'Deck jam switch, thermal overload',
+      'Mechanics safety switch, circuit breaker',
+      'Turret jam switch, circuit breaker',
+    ],
+    answer: 'Deck jam switch, Turret jam switch',
+  },
+  {
+    id: 17,
+    title: 'Gear Box',
+    question: 'What in the gear box drives the 1:1',
+    options: ['Worm shaft', '4:1', '2:1', 'Gear box clutch'],
+    answer: '2:1',
+  },
+  {
+    id: 18,
+    title: 'Gear Box',
+    question:
+      'What in the gear box controls the forward and rearward movement of the moving deck?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '1:1',
+  },
+  {
+    id: 19,
+    title: 'Gear Box',
+    question: 'What in the gear box disengages the gear box clutch?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '4:1',
+  },
+  {
+    id: 20,
+    title: 'Gear box',
+    question: 'What in the gear box drives the 4:1?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: 'Worm shaft',
+  },
+  {
+    id: 21,
+    title: 'Gear box',
+    question:
+      'What in the gear box controls the up and down movement of the deck assembly?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '2:1',
+  },
+  {
+    id: 22,
+    title: 'Gear box',
+    question: 'What in the gear box drives the detector?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '1:1',
+  },
+  {
+    id: 23,
+    title: 'Gear box',
+    question:
+      'What in the gear box controls the height of the rake throughout the pinsetter cycle?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '1:1',
+  },
+  {
+    id: 24,
+    title: 'Gear box',
+    question:
+      'What in the gear box controls the rearward and forward movement of the rake?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '4:1',
+  },
+  {
+    id: 25,
+    title: 'Gear box',
+    question:
+      'What in the gear box controls the opening and closing of the scissors?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '1:1',
+  },
+  {
+    id: 26,
+    title: 'Gear box',
+    question: 'What in the gear box drives the 2:1?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '4:1',
+  },
+  {
+    id: 27,
+    title: 'Gear box',
+    question:
+      'What in the gear box controls the height of the clutch reset lever during the pinsetter cycle?',
+    options: ['Worm shaft', '4:1', '2:1', '1:1'],
+    answer: '1:1',
+  },
+  {
+    id: 28,
+    title: 'Maintenance',
+    question:
+      'Should you forget how to install a belt you can check the following for a reminder',
+    options: [
+      'An adjacent machine',
+      'The service manual',
+      'The parts manual',
+      'All the above',
+    ],
+    answer: 'All the above',
+  },
+  {
+    id: 29,
+    title: 'Turret',
+    question: 'What is the indexing sequence for the turret?',
+    options: [
+      '5,8,7,4,2,1,3,6,10,9',
+      '1,2,3,4,5,6,7,8,9,10',
+      '9,10,6,3,1,2,4,7,8,5',
+      '9,10,6,2,1,3,4,7,8,5',
+    ],
+    answer: '9,10,6,3,1,2,4,7,8,5',
+  },
+  {
+    id: 30,
+    title: 'Turret',
+    question:
+      'The pin passes over the snubber, falls into the turret basket wire and strikes the ___ causing the turret to index.',
+    options: [
+      'Indexing trip lever',
+      'Indexing pedal',
+      'Indexing bell crank',
+      'Pin release lever',
+    ],
+    answer: 'Indexing trip lever',
+  },
+  {
+    id: 31,
+    title: 'Pinsetter',
+    question:
+      'The pinsetter is powered by an electric motor. What size is the motor?',
+    options: [
+      '1/2 horsepower',
+      '3/4 horsepower',
+      '1 horsepower',
+      '2 horsepower',
+    ],
+    answer: '1 horsepower motor',
+  },
+  {
+    id: 32,
+    title: 'Deck',
+    question: 'The deck holding hook is used for what purpose?',
+    options: [
+      'To hold the deck from setting new pins.',
+      'To keep the deck from lowering to detect pins during a 2nd ball cycle.',
+      'To keep the rake from sweeping until it is needed.',
+      'To keep the deck from lowering while the rake is sweeping.',
+    ],
+    answer: 'To keep the deck from lowering while the rake is sweeping',
+  },
+  {
+    id: 33,
+    title: 'Pin Gate',
+    question: 'The function of the pin gate is to:',
+    options: [
+      'To stop the pins at the top of the cross conveyor',
+      'To control the flow of pins into the turret',
+      'To allow pins to go into the turret',
+      'None of the above',
+    ],
+    answer: 'To control the flow of pins into the turret',
+  },
+  {
+    id: 34,
+    title: 'Pinsetter',
+    question: 'The function of the magnetic clutch is to:',
+    options: [
+      'To turn the pin wheel jackshaft',
+      'To turn the pin wheel',
+      'To turn the pin wheel drive pulley',
+      'To power the turret clutch',
+    ],
+    answer: 'To turn the pin wheel drive pulley',
+  },
+  {
+    id: 35,
+    title: 'Gear Box',
+    question: 'The gear box supplies power for the following:',
+    options: [
+      'Rake and deck',
+      'Cross conveyor and turret',
+      'Rake and turret',
+      'Deck and turret',
+    ],
+    answer: 'Rake and Deck',
+  },
+  {
+    id: 36,
+    title: 'Rake',
+    question: 'The rake begins its sweeping motion at what degree?',
+    options: ['45 degrees', '90 degrees', '270 degrees', '315 degrees'],
+    answer: '90 degrees',
+  },
+  {
+    id: 37,
+    title: 'Rake',
+    question: 'The rake finishes its sweeping motion at what degree?',
+    options: ['180 degrees', '225 degrees', '270 degrees', '315 degrees'],
+    answer: '180 degrees',
+  },
+  {
+    id: 38,
+    title: 'Maintenance',
+    question:
+      'What switch on the rear of the wire channel needs to be in the on position in order for the pinsetter to operate?',
+    options: [
+      'Main circuit breaker',
+      'Mechanics safety switch',
+      'Thermal overload',
+      'Managers control switch',
+    ],
+    answer: 'Mechanics safety switch',
+  },
 ];
